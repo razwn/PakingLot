695e1bf1d653aea08c3d084671c2b0a3f251ef0f
razvan-ritivoi <razvan.ritivoi@visma.com>
2022-11-21 09:42:16 +0200

    Created Entities

---
+++ src/main/java/com/parking/parkinglot/entities/User.java
@@ -0,0 +1,62 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class User {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    private String username;
+
+    @Basic
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private Collection<Car> oneToMany;
+
+    @OneToMany
+    public Collection<Car> getOneToMany() {
+        return oneToMany;
+    }
+
+    public void setOneToMany(Collection<Car> oneToMany) {
+        this.oneToMany = oneToMany;
+    }
+}
